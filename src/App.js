fa28dd444152fab7760b5926685a7c552499051b
Codyt11 <Codyt11@hotmail.com>
2022-01-14 00:06:10 -0600

    created collections, directory, header, menu, and sign in components and routed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
 import './App.css';
+
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component.jsx';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import Header from './components/header/header.component.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Routes>
+        <Route exact path='/' element={<HomePage/>} />
+        <Route path='/shop' element={<ShopPage/>} />
+        <Route path='/signin' element={<SignInAndSignUpPage/>} />
+      </Routes>
     </div>
   );
 }
